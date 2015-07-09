6f17b4d71f2ca2fb3ac9f341acee9f4ceeb59c2b
kurosanex <kurosanex5010@me.com>
2015-07-09 22:40:41 +0900

    [finish]gruff/show

--- app/controllers/gruff_controller.rb
+++ app/controllers/gruff_controller.rb
@@ -3,24 +3,18 @@ class GruffController < ApplicationController
   # require 'gnuplot'
   
   def show
-    # twitterid = params[:twitterid]
-    # date = params[:uuid]
+    twitterid = params[:twitterid]
+    uuid = params[:uuid]
 
-    # g = Gruff::Line.new
-    # g.title = "My Graph"
-
-    # g.theme = {:background_colors => %w[blue white]}
-
-    # g.data("data1", [1, 3, 3, 6, 4, 3])
-    # g.data("data2", [7, 8, 7, 10, 8, 9])
-    # g.data("data3", [1, 2, 3, 5, 9, 8])
-    # g.data("data4", [9, 9, 8, 9, 10, 9])
+    # twitterid = "sanex_now"
+    # uuid = "asdf"
     
-    # g.labels = {0 => '2010/01', 2 => '2010/03', 4 => '2010/05'}
+    @chart_data =[]
+    Sleepdata.where(twitterid: twitterid, uuid: uuid).find_each do |sdata|
+      @chart_data << [sdata[:created_at], sdata[:sleepdata]]
+    end
     
-    # send_data(g.to_blob, :type => 'image/png', :disposition=>'inline')
 
-    @chart_data = [['2014-04-01', 60], ['2014-04-02', 65], ['2014-04-03', 64]]
         
   end
 end
